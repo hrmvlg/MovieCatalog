3349a1ed902d90ffc9cbec1d54c4ca78933bdcfd
Olya Khromova <olya.hrmvlg@gmail.com>
2024-11-26 16:37:14 +0300

    Moved logic from App.jsx to Main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,13 @@ import './assets/index.css'
 import Header from "./layout/Header"
 import Footer from './layout/Footer';
 import Main from './layout/Main';
-import { useEffect, useState } from 'react';
 
 function App() {
-
-  const [movies, setMovies] = useState(null);
-
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: "Bearer " + import.meta.env.VITE_API_OPTIONS
-    }
-  };
-
-  useEffect(() => {
-    fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
-      .then(res => res.json())
-      .then(res => setMovies(res.results))
-      .catch(err => console.error(err));
-  }, [])
-
-  function onSearch(query, type) {
-    if (query) {
-      fetch(`https://api.themoviedb.org/3/search/${type}?query=${query}&include_adult=false&language=en-US&page=1`, options)
-        .then(res => res.json())
-        .then(res => setMovies(res.results))
-        .catch(err => console.error(err));
-
-    } else if (!query || query === '') {
-      if (type === 'multi') type = 'all'
-
-      fetch(`https://api.themoviedb.org/3/trending/${type}/day?language=en-US`, options)
-        .then(res => res.json())
-        .then(res => setMovies(res.results))
-        .catch(err => console.error(err));
-    }
-  }
 
   return (
     <>
       <Header />
-      <Main
-        movies={movies}
-        onSearch={onSearch} />
+      <Main />
       <Footer />
     </>
   )
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,8 +1,46 @@
 /* eslint-disable react/prop-types */
 import MovieList from '../components/MovieList';
 import Search from '../components/Search';
+import { useEffect, useState } from 'react';
 
-export default function Main({ movies, onSearch }) {
+const API_KEY = import.meta.env.VITE_API_OPTIONS;
+
+export default function Main() {
+
+    const [movies, setMovies] = useState(null);
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: "Bearer " + API_KEY
+        }
+    };
+
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+            .then(res => res.json())
+            .then(res => setMovies(res.results))
+            .catch(err => console.error(err));
+    }, [])
+
+    function onSearch(query, type) {
+        if (query) {
+            fetch(`https://api.themoviedb.org/3/search/${type}?query=${query}&include_adult=false&language=en-US&page=1`, options)
+                .then(res => res.json())
+                .then(res => setMovies(res.results))
+                .catch(err => console.error(err));
+
+        } else if (!query || query === '') {
+            if (type === 'multi') type = 'all'
+
+            fetch(`https://api.themoviedb.org/3/trending/${type}/day?language=en-US`, options)
+                .then(res => res.json())
+                .then(res => setMovies(res.results))
+                .catch(err => console.error(err));
+        }
+    }
+
     return (
         <>
             <div className="content">
